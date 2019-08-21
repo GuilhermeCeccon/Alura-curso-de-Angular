1ab0af07769d6b04ca2395d9afa23b083db9394b
guilherme.luz <guilherme.luz@compasso.com.br>
2019-08-21 17:25:29 -0300

    Angula Parte 1 - Fundamentos

---
+++ src/app/photos/photo-list/photo-list.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { PhotoService } from '../photo/photo.service';
+
+@Component({
+  selector: 'app-photo-list',
+  templateUrl: './photo-list.component.html',
+  styleUrls: ['./photo-list.component.css']
+})
+export class PhotoListComponent implements OnInit {
+
+  photos: any[] = [];
+
+  constructor(private photoService: PhotoService) { }
+
+  ngOnInit(): void {
+
+    this.photoService.listFromUser('falvio')
+      .subscribe(photos => this.photos = photos);
+  }
+
+}
